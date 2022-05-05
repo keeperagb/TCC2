2fbc1ab3981f4798576e7b077115453e38b358b4
keeperagb <alisson_gean18@hotmail.com>
2022-05-04 22:15:54 -0400

    Remove Link CSS
    Removido os links de css que estavam no componente

--- comtrato5/src/views/aulaComtrato.vue
+++ comtrato5/src/views/aulaComtrato.vue
@@ -1,5 +1,4 @@
 <template>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <menuNavBarVue />
   <div class="container col-md-6">
     <form action="">
